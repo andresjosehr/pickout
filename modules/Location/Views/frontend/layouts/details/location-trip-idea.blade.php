3ba3271545544e2f7e4fe56abef0c67880dcaa7d
Jose Andres <joseandreshernandezross@gmail.com>
2020-02-11 22:07:30 -0400

    Correccion realizada

--- modules/Location/Views/frontend/layouts/details/location-trip-idea.blade.php
+++ modules/Location/Views/frontend/layouts/details/location-trip-idea.blade.php
@@ -3,7 +3,7 @@
         <h3 class="py-5">{{__("Trip Ideas")}}</h3>
         @if(!empty($translation->trip_ideas))
             @php if(!is_array($translation->trip_ideas)) $translation->trip_ideas = json_decode($translation->trip_ideas); @endphp
-            <div class="row">
+            <div class="row justify-content-center">
                 @foreach($translation->trip_ideas as $key=>$trip_idea)
                     <div class="col-md-3 col-6 my-2 px-2 py-2">
                         <div class="cuadro-trip-div" data-url='{{$trip_idea['link']}}'>
