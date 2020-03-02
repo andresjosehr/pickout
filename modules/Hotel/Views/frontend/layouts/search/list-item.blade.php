54187733d621b0fff06f63da83ffa3c07a418e6a
Jose Andres <joseandreshernandezross@gmail.com>
2020-03-02 04:05:35 +0100

    Cambios realizados

--- modules/Hotel/Views/frontend/layouts/search/list-item.blade.php
+++ modules/Hotel/Views/frontend/layouts/search/list-item.blade.php
@@ -63,7 +63,7 @@
             </button>
         </div>
         <div style="height: 100%">
-            <iframe src="https://pickout.test/alojamientos?_layout=map" height="100%" width="100%" frameborder="0"></iframe>
+            <iframe src="https://pickout.cl/alojamientos?_layout=map" height="100%" width="100%" frameborder="0"></iframe>
         </div>
       </div>
     </div>
